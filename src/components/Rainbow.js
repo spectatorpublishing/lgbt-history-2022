b47ef9f1fad36614d3e74c56287fefccdc4daa32
hhuang <hwhuang01@gmail.com>
2022-09-28 20:02:57 -0400

    Refactored rainbow flag color tags.

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -3,36 +3,21 @@ import styled from 'styled-components';
 import { device } from '../device';
 import theme from '../theme';
 
-const TitleClosed = styled.div`
-    font-size: 4rem;
+const Title = styled.div`
+    font-size: ${props => props.open ? '3rem' : '4rem'};
     font-weight: bold;
-    font-family: 'Josefin-sans', sans-serif; 
     color: white;
-    margin: 2rem;
+    margin: 4rem;
     writing-mode: vertical-rl;
     transform: rotate(180deg);
     position: relative;
     float: right;
     display: flex;
-    // justify-content: center;
     align-self: center;
 `;
 
-const TitleOpen = styled.div`
-    font-size: 3rem;
-    font-weight: bold;
-    font-family: 'Josefin-sans', sans-serif; 
-    color: white;
-    margin: 2rem;
-    writing-mode: vertical-rl;
-    transform: rotate(180deg);
-    position: relative;
-    float: right;
-`
-
 const Container = styled.div`
     background-color: lavender;
-    // height: 120vh;
     display: flex;
     @media only screen and (max-width: 768px){
     }   
@@ -47,108 +32,65 @@ const Flag = styled.div`
     width: 100%;
 `;
 
-const RedClosed = styled.div`
-    background-color: ${theme.colors.lightRed};
-    // overflow: hidden;
+const Red = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedRed : theme.colors.lightRed};
     height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow: 1;
     &:hover {
         background-color: ${theme.colors.saturatedRed};
     }
 `;
 
-const RedOpen = styled.div`
-    background-color: ${theme.colors.saturatedRed};
-    // overflow: hidden;
+const Orange = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedOrange : theme.colors.lightOrange};
     height: auto;
-    width: 50%;
-    flex-grow: 1;
-`;
-
-const OrangeClosed = styled.div`
-    background-color: ${theme.colors.lightOrange};
-    height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow: 1;
     &:hover {
         background-color: ${theme.colors.saturatedOrange};
     }
 `;
 
-const OrangeOpen = styled.div`
-    background-color: ${theme.colors.saturatedOrange};
+const Yellow = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedYellow : theme.colors.lightYellow};
     height: auto;
-    width: 50%;
-    flex-grow: 1;
-`;
-
-const YellowClosed = styled.div`
-    background-color: ${theme.colors.lightYellow};
-    height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow: 1;
     &:hover {
         background-color: ${theme.colors.saturatedYellow};
     }
 `;
 
-const YellowOpen = styled.div`
-    background-color: ${theme.colors.saturatedYellow};
-    height: auto;
-    width: 50%;
-    flex-grow: 1;
 
-`;
-
-const GreenClosed = styled.div`
-    background-color: ${theme.colors.lightGreen};
+const Green = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedGreen : theme.colors.lightGreen};
     height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow:1;
     &:hover {
         background-color: ${theme.colors.saturatedGreen};
     }
 `;
 
-const GreenOpen = styled.div`
-    background-color: ${theme.colors.saturatedGreen};
+const Blue = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedBlue : theme.colors.lightBlue};
     height: auto;
-    width: 50%;
-    flex-grow: 1;
-`;
-
-const BlueClosed = styled.div`
-    background-color: ${theme.colors.lightBlue};
-    height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow: 1;
     &:hover {
         background-color: ${theme.colors.saturatedBlue};
     }
 `;
-const BlueOpen = styled.div`
-    background-color: ${theme.colors.saturatedBlue};
-    height: auto;
-    width: 50%;
-    flex-grow: 1;
-`;
 
-const PurpleClosed = styled.div`
-    background-color: ${theme.colors.lightPurple};
+const Purple = styled.div`
+    background-color: ${props => props.open ? theme.colors.saturatedPurple : theme.colors.lightPurple};
     height: auto;
-    width: auto;
+    width: ${props => props.open ? '50%' : 'auto'};
     flex-grow: 1;
     &:hover {
         background-color: ${theme.colors.saturatedPurple};
     }
-`;
-
-const PurpleOpen = styled.div`
-    background-color: ${theme.colors.saturatedPurple};
-    height: auto;
-    width: 50%;
-    flex-grow: 1;
 `;
 
 const Rainbow = ({ }) => {
@@ -158,23 +100,23 @@ return (
     <Container>
         <Flag>
             {currentOpen === 1 
-                ? <RedOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>News</TitleOpen></RedOpen> 
-                : <RedClosed onClick={() => setCurrentOpen(1)}><TitleClosed>News</TitleClosed></RedClosed>}
+                ? <Red open onClick={() => setCurrentOpen(-1)}><Title open>News</Title></Red> 
+                : <Red onClick={() => setCurrentOpen(1)}><Title>News</Title></Red>}
             {currentOpen === 2 
-                ? <OrangeOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>Opinion</TitleOpen></OrangeOpen> 
-                : <OrangeClosed onClick={() => setCurrentOpen(2)}><TitleClosed>Opinion</TitleClosed></OrangeClosed>}
+                ? <Orange open onClick={() => setCurrentOpen(-1)}><Title open>Opinion</Title></Orange> 
+                : <Orange onClick={() => setCurrentOpen(2)}><Title>Opinion</Title></Orange>}
             {currentOpen === 3 
-                ? <YellowOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>Sports</TitleOpen></YellowOpen> 
-                : <YellowClosed onClick={() => setCurrentOpen(3)}><TitleClosed>Sports</TitleClosed></YellowClosed>}
+                ? <Yellow open onClick={() => setCurrentOpen(-1)}><Title open>Sports</Title></Yellow> 
+                : <Yellow onClick={() => setCurrentOpen(3)}><Title>Sports</Title></Yellow>}
             {currentOpen === 4 
-                ? <GreenOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>Arts</TitleOpen></GreenOpen> 
-                : <GreenClosed onClick={() => setCurrentOpen(4)}><TitleClosed>Arts</TitleClosed></GreenClosed>}
+                ? <Green open onClick={() => setCurrentOpen(-1)}><Title open>Arts</Title></Green> 
+                : <Green onClick={() => setCurrentOpen(4)}><Title>Arts</Title></Green>}
             {currentOpen === 5 
-                ? <BlueOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>Entertainment</TitleOpen></BlueOpen> 
-                : <BlueClosed onClick={() => setCurrentOpen(5)}><TitleClosed>Entertainment</TitleClosed></BlueClosed>}
+                ? <Blue open onClick={() => setCurrentOpen(-1)}><Title open>Entertainment</Title></Blue> 
+                : <Blue onClick={() => setCurrentOpen(5)}><Title>Entertainment</Title></Blue>}
             {currentOpen === 6 
-                ? <PurpleOpen onClick={() => setCurrentOpen(-1)}><TitleOpen>The Eye</TitleOpen></PurpleOpen> 
-                : <PurpleClosed onClick={() => setCurrentOpen(6)}><TitleClosed>The Eye</TitleClosed></PurpleClosed>}
+                ? <Purple open onClick={() => setCurrentOpen(-1)}><Title open>The Eye</Title></Purple> 
+                : <Purple onClick={() => setCurrentOpen(6)}><Title>The Eye</Title></Purple>}
         </Flag>
     </Container>
 )};
