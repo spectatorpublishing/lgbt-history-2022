27741baa054baacf4a17760a2b46717f77759592
hhuang <hwhuang01@gmail.com>
2022-09-28 20:19:27 -0400

    Styled nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,7 @@ const VertNav = styled.div`
 `;
 
 const MenuItem = styled.div`
-    padding: 0.5rem 1rem;
+    padding: 1rem 2rem;
     a {
         text-decoration: none;
         color: ${theme.colors.white};
